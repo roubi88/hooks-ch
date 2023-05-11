9871a82571e8f24129143ca5fc6fd93718e8b2e5
samKallel <samKallel@hotmail.frgit config --global user.email 'samKallel@hotmail.fr>
2023-05-11 19:35:28 +0100

    part2 AddMovie

--- src/App.js
+++ src/App.js
@@ -2,11 +2,19 @@ import React, { useState } from 'react';
 import './App.css';
 import {moviesData} from './Component/MovieData/MovieData'
 import MovieListe from './Component/MovieListe/MovieListe';
+import AddMovie from './Component/AddMovie/AddMovie';
 
 function App() {
   const [movies, setmovies] = useState(moviesData)
+
+  const add=(newMovie)=>{
+    setmovies([...movies, newMovie]);
+  }
   return (
     <div className="App">
+     
+      <h2>CheckPoint Hooks : MovieApp</h2> 
+      <AddMovie add={add}/>
      <MovieListe movies={movies}/>
     </div>
   );
